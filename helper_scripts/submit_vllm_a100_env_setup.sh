c1cc8c15ceb1d4d498a2c516614f2ade36bc3623
shi676 <shi676@purdue.edu>
2026-05-04 14:41:25 -0400

    update anvil setup shell

--- helper_scripts/submit_vllm_a100_env_setup.sh
+++ helper_scripts/submit_vllm_a100_env_setup.sh
@@ -5,7 +5,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 ACCOUNT=""
 PARTITION="${SLURM_PARTITION:-gpu}"
-TIME_LIMIT="${TIME_LIMIT:-04:00:00}"
+TIME_LIMIT="${TIME_LIMIT:-06:00:00}"
 JOB_NAME="${JOB_NAME:-biollm-vllm-a100-env}"
 CPUS_PER_TASK="${CPUS_PER_TASK:-16}"
 
